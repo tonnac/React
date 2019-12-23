55aa4135818972a95add8577b367af75457cc3a9
tonnac <tonnac35@gmail.com>
2019-12-23 19:19:43 +0900

    2.2 Dynamic Component Generation

--- Projects/movie_app_2019/src/App.js
+++ Projects/movie_app_2019/src/App.js
@@ -1,17 +1,47 @@
 import React from "react";
 
 function Food(props) {
-  return <h1>I like {props.fav}</h1>;
+  return (
+    <div>
+      <h2>I like {props.name}</h2>
+      <img src={props.picture} width="50%" alt="" />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://www.koreanbapsang.com/wp-content/uploads/2016/10/DSC_1843-e1477360668451.jpg",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://i2.wp.com/www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg",
+  },
+  {
+    name: "Doncasu",
+    image: "https://jesswitkins.files.wordpress.com/2012/04/100_2023.jpg",
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/440px-Dolsot-bibimbap.jpg",
+  },
+  {
+    name: "Kimbap",
+    image:
+      "https://mykoreankitchen.com/wp-content/uploads/2006/10/1.-Easy-Kimbap.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
-      Hello!!!!
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chikumi" />
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
