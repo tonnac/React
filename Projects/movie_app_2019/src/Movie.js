edba65b5928f22a11f7255673752ddd7bee0c044
tonnac <tonnac35@gmail.com>
2019-12-25 12:04:13 +0900

    4.4 Styles Timelapse

--- Projects/movie_app_2019/src/Movie.js
+++ Projects/movie_app_2019/src/Movie.js
@@ -4,7 +4,7 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
+    <div className="movies__movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
