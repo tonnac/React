408f6e2b6cbcbdd48809a3b53dd37c2b039003dd
tonnac <tonnac35@gmail.com>
2019-12-25 11:22:34 +0900

    4.1 Rendering the Movies

--- Projects/movie_app_2019/src/App.js
+++ Projects/movie_app_2019/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,20 +11,41 @@ class App extends React.Component {
     };
   }
   async getMovies() {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=download_count",
+      // "https://yts.lt/api/v2/list_movies.json?sort_by=download_count",
+    );
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
   }
-  // getMovies = async () => {
-  //   const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-  // };
 
   componentDidMount() {
     this.getMovies();
-    
   }
+
+  renderMovies = () => {
+    const { movies } = this.state;
+    return movies.map(movie => {
+      return (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          year={movie.year}
+          title={movie.title}
+          summary={movie.summary}
+          poster={movie.medium_cover_image}
+        />
+      );
+    });
+  };
 
   render() {
     const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are Ready"}</div>;
+    return <div>{isLoading ? "Loading..." : this.renderMovies()}</div>;
   }
 }
 
