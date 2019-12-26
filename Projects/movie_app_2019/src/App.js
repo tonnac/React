3396710c4bede0c701191c197f0b18a22f643f22
tonnac <tonnac35@gmail.com>
2019-12-26 16:21:36 +0900

    MovieApp - 6.2 Building the Navigation

--- Projects/movie_app_2019/src/App.js
+++ Projects/movie_app_2019/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
+import Navgiation from "./components/Navigation";
 import About from "./routes/About";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <HashRouter>
+      <Navgiation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
