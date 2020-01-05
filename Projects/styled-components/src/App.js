91333bea42233edc6376affdcebfdd2fb3928fcb
tonnac <tonnac35@gmail.com>
2020-01-05 14:47:37 +0900

    Styled Components - 5. Extra Attributes and Mixins

--- Projects/styled-components/src/App.js
+++ Projects/styled-components/src/App.js
@@ -1,25 +1,17 @@
-import React, { Component, Fragment } from "react";
-import styled, { createGlobalStyle, keyframes, css } from "styled-components";
+import React from "react";
+import styled, { createGlobalStyle, css } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
     ${reset};
 `;
 
-function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <Container>
-        <Button>Hello</Button>
-        <Button danger rotationTime={2}>
-          Hello
-        </Button>
-        <Anchor href="http://google.com">Go to google</Anchor>
-      </Container>
-    </>
-  );
-}
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
 
 const Container = styled.div`
   height: 100vh;
@@ -27,39 +19,22 @@ const Container = styled.div`
   background-color: #bdc3c7;
 `;
 
-const rotation = keyframes`
-    from{
-      transform: rotate(0deg)
-    }
-    to{
-      transform: rotate(360deg)
-    }
-  `;
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
-
-  animation: ${props =>
-    props.danger
-      ? css`
-          ${rotation} ${props.rotationTime}s linear infinite
-        `
-      : ``};
+const Input = styled.input.attrs({
+  required: true
+})`
+  border-radius: 5px;
+  ${awesomeCard};
 `;
 
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
-`;
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Container>
+        <Input placeholder="Input your name." />
+      </Container>
+    </>
+  );
+}
 
 export default App;
