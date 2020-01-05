0fbbf952ed346f8b9c00871b42ff467214a95cd9
tonnac <tonnac35@gmail.com>
2020-01-05 14:55:29 +0900

    Styled Components 6. Theming

--- Projects/styled-components/src/App.js
+++ Projects/styled-components/src/App.js
@@ -1,40 +1,44 @@
 import React from "react";
-import styled, { createGlobalStyle, css } from "styled-components";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 import reset from "styled-reset";
+import theme from "./theme";
 
 const GlobalStyles = createGlobalStyle`
     ${reset};
 `;
-
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
-
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: #bdc3c7;
 `;
 
-const Input = styled.input.attrs({
-  required: true
-})`
-  border-radius: 5px;
-  ${awesomeCard};
+const Card = styled.div`
+  background-color: red;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
 `;
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <Container>
-        <Input placeholder="Input your name." />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 
 export default App;
---
+++ Projects/styled-components/src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+  mainColor: "#82ccdd",
+  dangerColor: "#eb2f06",
+  successColor: "#fad390"
+};
+
+export default theme;
