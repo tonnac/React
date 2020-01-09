2fc022fcf739ba4007a903ffd626cd2f5e13b76f
tonnac <tonnac35@gmail.com>
2020-01-10 02:06:49 +0900

    component ref

--- Projects/velopert/5.ref/src/App.js
+++ Projects/velopert/5.ref/src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
-function App() {
-  return <ValidationSample />;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={ref => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
