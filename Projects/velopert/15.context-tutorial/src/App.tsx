64fbb903df98936848aa49c28caadc90bfac142e
tonnac <tonnac35@gmail.com>
2020-01-09 14:49:41 +0900

    15 Provider

--- Projects/velopert/15.context-tutorial/src/App.tsx
+++ Projects/velopert/15.context-tutorial/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <ColorBox />
-    </div>
+    <ColorContext.Provider value={{ color: "red" }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 };
 
