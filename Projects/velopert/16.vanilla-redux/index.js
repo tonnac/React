a77daa4308470e904e95a928a77c6f3ec67401d9
tonnac <tonnac35@gmail.com>
2020-01-09 14:48:19 +0900

    ColorContext

---
+++ Projects/velopert/16.vanilla-redux/index.js
@@ -0,0 +1,76 @@
+import { createStore } from "redux";
+
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+function toggleSwitch() {
+  return { type: TOGGLE_SWITCH };
+}
+function increase(difference) {
+  return { type: INCREASE, difference };
+}
+
+function decrease() {
+  return { type: DECREASE };
+}
+
+const initialState = {
+  toggle: false,
+  counter: 0
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+function render() {
+  const state = store.getState();
+
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+  counter.innerText = state.counter;
+}
+
+render();
+store.subscribe(render);
+
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+};
+
+btnDecrease.onclick = () => {
+  store.dispatch(decrease());
+};
