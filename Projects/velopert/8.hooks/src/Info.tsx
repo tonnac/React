c499df9775032f4057ebeeb748e291cacd60a77d
tonnac <tonnac35@gmail.com>
2020-01-13 23:16:16 +0900

    velopert - 8.2 useeffect

--- Projects/velopert/8.hooks/src/Info.tsx
+++ Projects/velopert/8.hooks/src/Info.tsx
@@ -1,4 +1,4 @@
-import React, { useState, SyntheticEvent } from "react";
+import React, { useState, useEffect } from "react";
 
 function Info() {
   const [name, setName] = useState("");
@@ -10,6 +10,26 @@ function Info() {
   const onChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNickname(e.target.value);
   };
+  useEffect(() => {
+    console.log("렌더링 완료");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  });
+  useEffect(() => {
+    console.log("마운트 될때만 실행");
+    return () => {
+      console.log("언마운트 될때만 실행");
+    };
+  }, []);
+  useEffect(() => {
+    console.log("이름 바뀔때만");
+  }, [name]);
+  useEffect(() => {
+    console.log("닉네임 바뀔때만");
+  }, [nickName]);
   return (
     <div>
       <div>
