67f4f36b976e6e2d4226048594b8d21a1245aa7f
tonnac <tonnac35@gmail.com>
2020-01-19 16:50:35 +0900

    Commit before eject

---
+++ Projects/velopert/20.ssr-recipe/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Menu from "./components/Menu";
+import { Route } from "react-router-dom";
+import RedPage from "./pages/RedPage";
+import BluePage from "./pages/BluePage";
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      <hr />
+      <Route path="/red" component={RedPage} />
+      <Route path="/blue" component={BluePage} />
+    </div>
+  );
+}
+
+export default App;
---
+++ Projects/velopert/20.ssr-recipe/src/pages/BluePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Blue from "../components/Blue";
+
+export default function BluePage() {
+  return (
+    <div>
+      <Blue />
+    </div>
+  );
+}
